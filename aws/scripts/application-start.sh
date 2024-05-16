d64a1fa953310d629af78bee6261c2dfe5593b8d
dave.ma <fei@coinfloww.com>
2024-05-16 17:52:41 +0800

    update

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+APP_NAME=""
+IMAGE=""
+
 echo "app is $APP_NAME "
 
 docker run -d --restart=always --name ${APP_NAME}  $IMAGE
--- aws/scripts/application-stop.sh
+++ aws/scripts/application-stop.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
+APP_NAME=""
+
 echo "app is $APP_NAME "
 docker rm -f ${APP_NAME}
