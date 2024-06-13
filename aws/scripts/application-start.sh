13f6f979c645aee7a9177af001cf181a0bb9f229
dave.ma <fei@coinfloww.com>
2024-06-13 17:25:02 +0800

    update

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -2,5 +2,5 @@
 APP_NAME=""
 IMAGE=""
 
-
+aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 580167004110.dkr.ecr.ap-southeast-1.amazonaws.com
 docker run -d --name ${APP_NAME}  $IMAGE
